f7e52f05d86050ac78625647c5aedc3c86f9b573
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2020-02-18 14:31:12 -0300

    ng g interface models/car

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Customer } from './customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  public firstPage: string = "";
+  public prevPage: string = "";
+  public nextPage: string = "";
+  public lastPage: string = "";
+  //customers - https://api.myjson.com/bins/16gfi8?pretty=1
+  apiURL: string = 'https://api.myjson.com/bins/16gfi8';
+  constructor(private httpClient: HttpClient) { }
+  public createCustomer(customer: Customer) { }
+  public updateCustomer(customer: Customer) { }
+  public deleteCustomer(id: number) { }
+  public getCustomerById(id: number) { }
+  public getCustomers(url?: string) { }
 
-  constructor() { }
+  public getContacts() {
+    return this.httpClient.get<Customer[]>(`${this.apiURL}`);
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from  '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
@@ -20,7 +20,8 @@ import { CustomerUpdateComponent } from './customer-update/customer-update.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
